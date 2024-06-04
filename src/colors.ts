d0d5e70f54bfd60e2bf54ecfbe7ff36325a8e519
Arseniy Zakharchenko <arseniyboss5@gmail.com>
2024-06-04 08:54:13 +0200

    change blue color

--- src/colors.ts
+++ src/colors.ts
@@ -4,5 +4,5 @@ export const colors: Color[] = [
   { value: '#888', texture: '/textures/lightgrey.jpg' },
   { value: '#666', texture: '/textures/darkgrey.jpg' },
   { value: '#837a6e', texture: '/textures/golden.jpg' },
-  { value: '#474c5a', texture: '/textures/blue.jpg' },
+  { value: '#374c5a', texture: '/textures/blue.jpg' },
 ]
